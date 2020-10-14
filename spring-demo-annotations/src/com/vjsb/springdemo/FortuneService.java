798db5c74215c3b0cf33a216ec47973a27ff9013
Vimaljeet Singh <jeetvimal98singh@gmail.com>
2020-10-14 09:08:55 +0530

    Completed till Bean Scopes and Lifecycle Methods

---
+++ spring-demo-annotations/src/com/vjsb/springdemo/FortuneService.java
@@ -0,0 +1,6 @@
+package com.vjsb.springdemo;
+
+public interface FortuneService {
+
+	public String getFortune();
+}
---
+++ spring-demo-annotations/src/com/vjsb/springdemo/RESTFortuneService.java
@@ -0,0 +1,14 @@
+package com.vjsb.springdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RESTFortuneService implements FortuneService {
+
+	@Override
+	public String getFortune() {
+		
+		return null;
+	}
+
+}
